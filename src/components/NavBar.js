f1cf058a4d170b05c73380cf917a8443292a4600
Phong <huynhthanhphong0931@gmail.com>
2022-04-10 15:13:24 +0700

    Update Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -53,7 +53,6 @@ export default function NavBar() {
                             sx={{
                                 pt: 3,
                                 pb: 2,
-                                px: 2.5,
                                 flexShrink: 0,
                             }}
                         >
@@ -62,49 +61,20 @@ export default function NavBar() {
                                 alignItems="center"
                                 justifyContent="space-between"
                             >
-                                {/* <Typography variant="h5">News</Typography> */}
-                                <Logo />
 
+                                <Logo />
                                 <IconButton>
                                     <BiLeftArrow />
                                 </IconButton>
                             </Stack>
                         </Stack>
-                        <Typography>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Explicabo autem dolore temporibus dolorum
-                            maxime dignissimos ipsam voluptate dolores natus,
-                            rerum ex voluptates, aperiam perspiciatis soluta
-                            excepturi eius laborum atque veniam nobis,
-                            consequuntur fugiat? Veritatis ipsum asperiores
-                            quasi natus ipsam aspernatur recusandae quia
-                            laudantium optio cumque expedita exercitationem
-                            ullam quas fugit, in odit iusto nostrum suscipit
-                            adipisci. Itaque, ipsa? Aliquid itaque veritatis
-                            distinctio numquam soluta nesciunt aspernatur dolor
-                            voluptate? Est animi excepturi nobis nihil nostrum
-                            deleniti architecto, illo pariatur fugit. Ad, culpa!
-                            Sunt, quia saepe! Quis quidem tenetur error voluptas
-                            facilis quo eum veritatis obcaecati nemo. Molestias
-                            perferendis, vero neque, reprehenderit magnam atque
-                            repudiandae itaque odit esse delectus pariatur
-                            doloribus quos dolore, nesciunt quisquam tempora!
-                            Eveniet rem aliquam voluptas sapiente vero id,
-                            voluptates dolorum non vitae sit repudiandae velit
-                            reiciendis quod animi quisquam! Est consectetur
-                            laudantium dignissimos! Rem sequi repellat rerum
-                            facilis eveniet fugit neque veritatis illo
-                            accusantium voluptatum, unde ipsum sint accusamus
-                            nemo atque maxime explicabo esse voluptates tempore
-                            doloribus? Exercitationem eius dolore tempore
-                            aliquid reiciendis ex repellat inventore amet
-                            commodi ea culpa, corrupti eos perferendis sint
-                            assumenda, labore illo cumque fugiat reprehenderit.
-                            Pariatur quaerat veniam excepturi optio obcaecati
-                            quo rerum nesciunt, cumque molestias corporis hic
-                            quasi illo totam. Officiis.
+                        <Typography sx={{
+                            // textOverflow: 'ellipsis',
+                            // overflow: 'hidden',
+                            // whiteSpace: 'nowrap',
+                        }}>
+                            Call by value & Call by reference trong C++
                         </Typography>
-                        <Box sx={{ flexGrow: 1 }} />
                     </Container>
                 </Scrollbar>
             </RootStyle>
